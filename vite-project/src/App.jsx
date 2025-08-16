68c1d0fffed9227b2c2aa11bfdf5c3ba18913a4c
Ann <annrosemk227@gmail.com>
2025-08-16 18:15:53 +0530

    Designing structure

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1>TO DO List</h1>
+      <div className="input-container">
+        <input type="text" />
+        <button>+</button>
+      </div>
+
+      <div className="container">
+        <div className="todo">
+          <p>Complete React</p>
+          <div className="actions">
+            <input type="checkbox" />
+            <button className="delete">Delete</button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
